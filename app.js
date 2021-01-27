8a6a9b4abe62953d36f94e6adba995b40a2dcefa
fahimdipto <sultanparvez77.sp@gmail.com>
2021-01-28 03:09:04 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,114 @@
+const game = ()=> {
+	let pScore = 0;
+	let cScopre =0;
+
+	//start game
+	const startGame = ()=>{
+		const playBtn = document.querySelector('.intro button'); 
+		const introScreen =document.querySelector('.intro'); 
+		const match = document.querySelector('.match'); 
+		playBtn.addEventListener('click',()=>{
+			introScreen.classList.add("fadeOut");
+			match.classList.add("fadeIn");
+		});
+	};
+	//actual game
+	const playMatch =() =>{
+		const options = document.querySelectorAll('.options button');
+		const playerHand = document.querySelector('.player-hand');
+		const computerHand = document.querySelector('.computer-hand');
+		const hands = document.querySelectorAll('.hands img');
+		hands.forEach (hand =>{
+			hand.addEventListener('animationend',function(){
+				this.style.animation="";
+			});
+		});
+		//computer options 
+		const computerOptions =['rock','paper','scissors'];
+		options.forEach(option=>{
+			option.addEventListener('click', function () {
+				//computer choice
+				const computerNumber = Math.floor(Math.random() * 3);
+				const computerChoice=computerOptions[computerNumber];
+
+				setTimeout(()=>{
+				compareHands(this.textContent,computerChoice);
+
+				// update image
+				playerHand.src=`./assets/${this.textContent}.png`;
+				computerHand.src=`./assets/${computerChoice}.png`;
+				},2000);
+				playerHand.style.animation = "shakePlayer 2s ease";
+				computerHand.style.animation="shakeComputer 2s ease";
+			});
+
+		});
+		
+	};
+	const updateScore = () =>{
+		const playerScore= document.querySelector('.player-score p');
+		const computerScore= document.querySelector('.computer-score p');
+		playerScore.textContent=pScore;
+		computerScore.textContent=cScopre;
+
+	
+	}
+
+	const compareHands = (playerChoice,computerChoice) =>{
+		const winner = document.querySelector('.winner');
+		if(playerChoice===computerChoice){
+			winner.textContent="it's a tie";
+			return;
+		}
+		if(playerChoice==='rock'){
+			if (computerChoice==='scissors'){
+				winner.textContent="you Win";
+				pScore++;
+				updateScore();
+				return
+			}
+			else{
+				winner.textContent="you lost";
+				cScopre++;
+				updateScore();
+				return
+			}
+		}
+		if(playerChoice==='scissors'){
+			if (computerChoice==='paper'){
+				winner.textContent="you Win";
+				pScore++;
+				updateScore();
+				return
+			}
+			else{
+				winner.textContent="you lost";
+				cScopre++;
+				updateScore();
+				return
+			}
+		}
+		if(playerChoice==='paper'){
+			if (computerChoice==='scissors'){
+				winner.textContent="you lost";
+				cScopre++;
+				updateScore();
+				return
+			}
+			else{
+				winner.textContent="you win";
+				pScore++;
+				updateScore();
+				return
+			}
+		}
+
+	};
+
+	// call all the inner function
+	startGame();
+	playMatch();
+
+};
+//start the game
+game();
